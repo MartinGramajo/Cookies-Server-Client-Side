f3c773cb60c60e925c246af3e95de6bf0c645585
Martin <martingramajo08@gmail.com>
2024-11-05 13:38:55 -0300

    Shopping Cart server side

--- src/components/TopMenu.tsx
+++ src/components/TopMenu.tsx
@@ -1,7 +1,31 @@
-import React from "react";
-import { CiBellOn, CiChat1, CiMenuBurger, CiSearch, CiShoppingBasket } from "react-icons/ci";
+import { cookies } from "next/headers";
+import {
+  CiChat1,
+  CiMenuBurger,
+  CiSearch,
+  CiShoppingBasket,
+} from "react-icons/ci";
 
-const TopMenu = () => {
+const getTotalCount = (cart: { [id: string]: number }): number => {
+  let items = 0; // El numero que vamos a mostrar
+
+  Object.values(cart).forEach((value) => {
+    items += value as number; // aquí sumamos los valores que tienen los productos en el carrito
+  });
+
+  return items;
+};
+
+const TopMenu = async () => {
+  const cookieStore = await cookies();
+
+  // esto son los objetos de nuestra estructura por ende tenemos que contar los valores
+  const cart = JSON.parse(cookieStore.get("cart")?.value ?? "{}");
+
+  // guardamos el resultado en una variable
+
+  const totalItems = getTotalCount(cart);
+
   return (
     <div className="sticky z-10 top-0 h-16 border-b bg-white lg:py-2.5">
       <div className="px-6 flex items-center justify-between space-x-4">
@@ -34,7 +58,12 @@ const TopMenu = () => {
             <CiChat1 size={25} />
           </button>
           <button className="flex items-center justify-center p-2 h-10 rounded-xl border bg-gray-100 focus:bg-gray-100 active:bg-gray-200">
-            <span className="text-sm mr-2 text-blue-800 font-bold">10</span>
+            {totalItems > 0 && (
+              <span className="text-sm mr-2 text-blue-800 font-bold">
+                {totalItems}
+              </span>
+            )}
+
             <CiShoppingBasket size={25} />
           </button>
         </div>
